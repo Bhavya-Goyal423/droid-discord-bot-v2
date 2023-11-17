bb5c57dfba4b97fb1b0cbcd435c925c408b680e6
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-11-17 18:35:16 +0530

    added functionality to disable autorole and welcome

---
+++ commands/disable/disable.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const guildModel = require("../../models/GuildSchema");
+
+module.exports = {
+  run: async ({ interaction, client }) => {
+    await interaction.deferReply({ ephemeral: true });
+    const subCommand = interaction.options.getSubcommand();
+    const guild = await guildModel.findOne({ guildId: interaction.guildId });
+    const guildWelcome = Object.fromEntries(guild.welcome);
+
+    // * ------------- Disable welcome ---------------------------
+
+    if (subCommand === "welcome") {
+      guild.welcome = { ...guildWelcome, channelId: null, message: null };
+      await guild.save();
+      return interaction.followUp({ content: "Disabled welcome ✅" });
+    }
+
+    // * ------------- Disable autorole ---------------------------
+
+    if (subCommand === "auto_role") {
+      guild.welcome = { ...guildWelcome, roleId: null };
+      await guild.save();
+      return interaction.followUp({ content: "Disabled Auto Role ✅" });
+    }
+  },
+  data: new SlashCommandBuilder()
+    .setName("disable")
+    .setDescription("disables a specific thing")
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName("welcome")
+        .setDescription(
+          "disables the welcome message and disassociate the channel"
+        )
+    )
+    .addSubcommand((subCommand) =>
+      subCommand.setName("auto_role").setDescription("disables the autorole")
+    ),
+
+  MemberPermissions: [
+    PermissionFlagsBits.ManageChannels,
+    PermissionFlagsBits.ManageRoles,
+  ],
+  BotPermissions: [
+    PermissionFlagsBits.ManageChannels,
+    PermissionFlagsBits.ManageRoles,
+  ],
+};
