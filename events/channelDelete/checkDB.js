7f9bd2a11c175a07c44382a466130d6ed3fcd2c4
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-11-18 14:11:06 +0530

    sends a message to the owner whenever the channel associated with welcome or levels log is deleted

---
+++ events/channelDelete/checkDB.js
@@ -0,0 +1,32 @@
+const guildModel = require("../../models/GuildSchema");
+
+module.exports = async (channel, client) => {
+  try {
+    const channelId = channel.id;
+    console.log("CHANNEL_ID", channelId);
+    console.log("GUILD_ID", channel.guildId);
+    const guild = await guildModel.findOne({ guildId: channel.guildId });
+    const guildWelcome = Object.fromEntries(guild.welcome);
+    const welcomeChannelId = guildWelcome.channelId;
+    const logChannelId = guild.levelLogChannelId;
+
+    const owner = channel.guild.members.cache.get(channel.guild.ownerId);
+
+    if (welcomeChannelId === channelId) {
+      guild.welcome = { ...guildWelcome, channelId: null };
+      await guild.save();
+      owner.send(
+        `The channel deleted (${channel.name}) was assigned for welcome message and now has been set to null,to assign channel again use /set welcome channel`
+      );
+    }
+    if (logChannelId === channelId) {
+      guild.levelLogChannelId = null;
+      await guild.save();
+      owner.send(
+        `The channel deleted (${channel.name}) was assigned for logging levels and now has been set to null,to assign channel again use /set level log_channel`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
