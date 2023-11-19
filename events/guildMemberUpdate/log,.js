125482529c8a4b06b0d4dd36bf6acf7e31724feb
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-11-19 13:54:49 +0530

    added functionality to log whenever user roles get updated

--- events/guildMemberUpdate/log,.js
+++ events/guildMemberUpdate/log,.js
@@ -1,4 +1,72 @@
-module.exports = (oldMember, newMember) => {
-  console.log(oldMember.roles);
-  console.log(newMember.roles);
+const { Constants } = require("discord.js");
+const moment = require("moment");
+const guildModel = require("../../models/GuildSchema");
+
+module.exports = async (oldMember, newMember) => {
+  try {
+    const guild = await guildModel.findOne({ guildId: newMember.guild.id });
+    if (!guild.logChannelId) return;
+    const user = newMember.user;
+    const addedRole = newMember.roles.cache.find(
+      (role) => !oldMember.roles.cache.has(role.id)
+    );
+    const removedRole = oldMember.roles.cache.find(
+      (role) => !newMember.roles.cache.has(role.id)
+    );
+
+    const logChannel = newMember.guild.channels.cache.get(guild.logChannelId);
+    if (!logChannel) return;
+
+    if (addedRole) {
+      const embed = {
+        color: 0xf05941,
+        author: {
+          name: user.username,
+          icon_url: user.displayAvatarURL({ size: 128 }),
+        },
+        description: `**${user} has been updated**`,
+        fields: [
+          {
+            name: "**Roles added**",
+            value: `:white_check_mark: ${addedRole?.name}`,
+          },
+        ],
+        thumbnail: {
+          url: user.displayAvatarURL({ size: 128 }),
+        },
+        footer: {
+          text: `${newMember.guild.name} • ${moment(new Date()).format(
+            "YYYY/MM/DD h:mm A"
+          )}`,
+        },
+      };
+      logChannel.send({ embeds: [embed] });
+    } else if (removedRole) {
+      const embed = {
+        color: 0xf05941,
+        author: {
+          name: user.username,
+          icon_url: user.displayAvatarURL({ size: 128 }),
+        },
+        description: `**${user} has been updated**`,
+        fields: [
+          {
+            name: "**Roles removed**",
+            value: `:no_entry: ${removedRole?.name}`,
+          },
+        ],
+        thumbnail: {
+          url: user.displayAvatarURL({ size: 128 }),
+        },
+        footer: {
+          text: `${newMember.guild.name} • ${moment(new Date()).format(
+            "YYYY/MM/DD h:mm A"
+          )}`,
+        },
+      };
+      logChannel.send({ embeds: [embed] });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
